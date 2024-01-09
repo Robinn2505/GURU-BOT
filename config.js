0c7314dd5bbda34d02bf0fd06459a7e0c9ff8079
Robinn2505 <149889813+Robinn2505@users.noreply.github.com>
2024-01-09 13:49:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -52,8 +52,8 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = '𝚁𝙾𝙱𝙸𝙽'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'ROBIN' 
+global.author = '@THE LOST UCHIHA' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
