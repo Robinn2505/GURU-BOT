7dead971fe7bdfd8780de4d98316c22fa3184abc
Robinn2505 <149889813+Robinn2505@users.noreply.github.com>
2024-01-17 10:20:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ import axios from 'axios'
 
 global.owner = [
   ['919685031377', '𝚁𝙾𝙱𝙸𝙽', true],
-  ['6281255369012', '𝚁𝙾𝙱𝙸𝙽', true], 
+  ['917523808860', '𝚁𝙾𝙱𝙸𝙽', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['919685031377','917523808860'] 
+global.prems = ['919685031377', '917523808860', '919685031377']
+global.allowed = ['919685031377', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
